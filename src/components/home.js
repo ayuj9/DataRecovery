d8aa48b84e09548e1baada7cad38ba72d3def9d1
ayuj9 <ayuj5571@gmail.com>
2024-12-14 19:53:03 +0530

    new

--- src/components/home.js
+++ src/components/home.js
@@ -1,18 +1,27 @@
-import { Typography, Grid } from "@mui/material";
+import { Typography, Grid, CardActionArea } from "@mui/material";
 import MemoryIcon from "@mui/icons-material/Memory";
 import PhoneInTalkIcon from "@mui/icons-material/PhoneInTalk";
 import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
 import SupportAgentIcon from "@mui/icons-material/SupportAgent";
 import TypesTable from "./table";
 import LastSection from "./LastSection";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+
 import "../App.css";
-const imagePath = process.env.PUBLIC_URL + "/design.png";
+// const imagePath = process.env.PUBLIC_URL + "/design.png";
+const harddisk = process.env.PUBLIC_URL + "/harddrive1.png";
+const virus = process.env.PUBLIC_URL + "/virus.jpg";
+const nofile = process.env.PUBLIC_URL + "/nofile.jpg";
+const error = process.env.PUBLIC_URL + "/error.png";
+const bin = process.env.PUBLIC_URL + "/bin.png";
 
 export default function Home() {
   const box_style = {
     width: { xs: "400px" },
     height: { xs: "170px", lg: "140px" },
-    border: "3px solid #cdecf8",
+    border: "3px solid #0056a4 ",
     borderRadius: "90px",
     display: "flex",
     justifyContent: "center",
@@ -23,7 +32,7 @@ export default function Home() {
     flexDirection: "column",
     fontSize: { lg: "23px", xs: "28px" },
     fontFamily: "initial",
-    backgroundColor: "#cdecf8",
+    backgroundColor: "#cdecf8 ",
     marginBottom: "40px",
   };
   const block_style = {
@@ -34,7 +43,12 @@ export default function Home() {
     flexDirection: "column",
     fontSize: "20px",
     fontFamily: "initial",
-    // backgroundColor: "#b8dcfd ",
+  };
+
+  const text_style = {
+    fontFamily: "roboto",
+    fontSize: { lg: "40px", xs: "20px" },
+    fontWeight: "bold",
   };
 
   return (
@@ -64,25 +78,33 @@ export default function Home() {
       </Grid>
       <Grid
         container
-        className="no-text-select"
+        className="bg-home"
         direction="row"
         sx={{ width: { xs: "410px", lg: "1450px" } }}
       >
         <Grid
           container
-          className="no-text-select"
           direction="column"
           sx={{
             width: { lg: "840px", xs: "410px" },
-            backgroundColor: "#3696ef",
           }}
           display="flex"
           justifyContent="center"
           paddingLeft="40px"
         >
-          <Grid item>
+          <Grid item className="no-text-select">
             <Typography
-              color="white"
+              color="red"
+              fontSize="18px "
+              fontFamily="inherit"
+              fontWeight="bold"
+            >
+              PROFESSIONAL HDD RECOVERY SERVICE
+            </Typography>
+          </Grid>
+          <Grid item className="no-text-select">
+            <Typography
+              color="black"
               fontSize="70px "
               fontFamily="roboto"
               fontWeight="bold"
@@ -90,14 +112,14 @@ export default function Home() {
               Data Emergency! <br />
             </Typography>
           </Grid>
-          <Grid item>
+          <Grid item className="no-text-select">
             <Typography
-              color="white"
-              fontSize="40px"
+              color="#474646"
+              fontSize="34px"
               fontFamily="roboto"
               fontWeight="bold"
             >
-              Fast and Reliable Hard Drive Data Recovery Starts Here."
+              "Fast and Reliable Hard Drive Data Recovery <br /> Starts Here"
             </Typography>
           </Grid>
           <Grid
@@ -116,6 +138,7 @@ export default function Home() {
               display: "flex",
               alignItems: "center",
               justifyContent: "center",
+              boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
             }}
           >
             <PhoneInTalkIcon /> Call for a free Evaluation <br /> Ph:
@@ -127,24 +150,29 @@ export default function Home() {
           lg={5}
           sm={12}
           sx={{
-            backgroundImage: `url(${imagePath})`,
-            backgroundColor: "#3696ef",
+            backgroundImage: `url(${harddisk})`,
+            // backgroundColor: "#3696ef",
             height: { lg: "500px", xs: "400px" },
             width: { lg: "1440", xs: "530px" },
+            // borderRadius: "10px", // Adjust radius as needed
+            // overflow: "hidden",
           }}
-        ></Grid>
+        />
       </Grid>
       <Grid
         container
-        className="no-text-select"
+        classname="bg-home"
+        // className="no-text-select"
         direction="row"
         sx={{
           // backgroundColor: "#cdecf8",
+
           width: { xs: "410px", lg: "1450px" },
         }}
-        columnGap={4}
+        columnGap={6}
         rowGap={4}
-        paddingLeft="30px"
+        paddingLeft="60px"
+        paddingBottom="16px"
       >
         <Grid
           item
@@ -152,40 +180,179 @@ export default function Home() {
           xs={12}
           sx={{
             display: "flex",
-            height: { xs: "350px", lg: "240px" },
-            justifyContent: "center",
+            height: { xs: "350px", lg: "170px" },
+            // justifyContent: "center",
             letterSpacing: "1px",
           }}
         >
           <Typography
-            paddingTop="100px"
+            paddingTop="60px"
+            pl={5}
             fontSize="36px"
-            fontFamily="roboto"
+            fontFamily=""
             fontWeight="bold"
           >
             Regardless of the Reason Behind Your Data Loss, We are Your Solution
           </Typography>
         </Grid>
-        <Grid item lg={2} sm={12} sx={box_style}>
-          Deleted or lost partition on hard disk
-        </Grid>
-        <Grid item lg={2} sm={12} sx={box_style}>
-          Unexpectedly format the hard drive
-        </Grid>
-        <Grid item lg={2} sm={12} sx={box_style}>
-          Computer viruses
-        </Grid>
-        <Grid item lg={2} sm={12} sx={box_style}>
-          Unexpected power cut
-        </Grid>
-        <Grid item lg={2} sm={12} sx={box_style}>
-          Other 100+ data loss scenarios
-        </Grid>
-      </Grid>
 
+        <Card
+          sx={{
+            maxWidth: 300,
+            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
+            border: "2px solid transparent",
+            "&:hover::before": {
+              opacity: 1, // Show gradient on hover
+            },
+            "&:hover": {
+              borderColor: "black",
+              transform: "scale(1.03)", // Optional zoom effect
+              transition: "transform 0.3s",
+
+              boxShadow: "0px 8px 20px rgba(0, 0, 0, 0.3)", // Add shadow on hover
+            },
+          }}
+        >
+          <CardActionArea>
+            <CardMedia sx={{ height: 160, backgroundImage: `url(${bin})` }} />
+            <CardContent>
+              <Typography gutterBottom variant="h6" component="div">
+                Accidental Deletion
+              </Typography>
+              <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                Accidental deletions, whether from a mistaken SHIFT+DEL or
+                clearing the Recycle Bin, are common occurrences. For complex
+                data loss situations, trust our professionals to restore your
+                hard disk data.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <Card
+          sx={{
+            maxWidth: 300,
+            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
+            border: "2px solid transparent",
+            "&:hover::before": {
+              opacity: 1, // Show gradient on hover
+            },
+            "&:hover": {
+              borderColor: "black",
+              transform: "scale(1.03)", // Optional zoom effect
+              transition: "transform 0.3s",
+
+              boxShadow: "0px 8px 20px rgba(0, 0, 0, 0.3)", // Add shadow on hover
+            },
+          }}
+        >
+          <CardActionArea>
+            <CardMedia
+              sx={{ height: 160, backgroundImage: `url(${nofile})` }}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h6" component="div">
+                Data loss Due to Drive Formatting
+              </Typography>
+              <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                Formatting a hard drive partition is a common cause of data
+                loss, especially when drives become slow or are impacted by
+                malware.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <Card
+          sx={{
+            maxWidth: 300,
+            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
+            border: "2px solid transparent",
+            "&:hover::before": {
+              opacity: 1, // Show gradient on hover
+            },
+            "&:hover": {
+              borderColor: "black",
+              transform: "scale(1.03)", // Optional zoom effect
+              transition: "transform 0.3s",
+
+              boxShadow: "0px 8px 20px rgba(0, 0, 0, 0.3)", // Add shadow on hover
+            },
+          }}
+        >
+          <CardActionArea>
+            <CardMedia sx={{ height: 160, backgroundImage: `url(${virus})` }} />
+            <CardContent>
+              <Typography gutterBottom variant="h6" component="div">
+                Computer viruses
+              </Typography>
+              <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                Viruses can severely damage data integrity by corrupting or
+                deleting files. If your HDD data, files, or folders are affected
+                by a virus, we can help recover them
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card
+          sx={{
+            maxWidth: 300,
+            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
+            border: "2px solid transparent",
+            "&:hover::before": {
+              opacity: 1, // Show gradient on hover
+            },
+            "&:hover": {
+              borderColor: "black",
+              transform: "scale(1.03)", // Optional zoom effect
+              transition: "transform 0.3s",
+
+              boxShadow: "0px 8px 20px rgba(0, 0, 0, 0.3)", // Add shadow on hover
+            },
+          }}
+        >
+          <CardActionArea>
+            <CardMedia sx={{ height: 160, backgroundImage: `url(${error})` }} />
+            <CardContent>
+              <Typography gutterBottom variant="h6" component="div">
+                Unexpected power cut
+              </Typography>
+              <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                Viruses can severely damage data integrity by corrupting or
+                deleting files. If your HDD data, files, or folders are affected
+                by a virus, we can help recover them
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+      <Grid
+        item
+        sx={{
+          height: { lg: "150px" },
+          width: { lg: "1450px", xs: "350px" },
+          fontFamily: "roboto",
+          fontSize: { lg: "40px", xs: "20px" },
+          fontWeight: "bold",
+          textAlign: "center",
+          alignItems: "center",
+          letterSpacing: "2px",
+          display: "flex",
+          justifyContent: "center",
+          paddingTop: "10px",
+        }}
+      >
+        <Typography sx={text_style}>Higher</Typography>
+        <Typography color="red" sx={text_style}>
+          &nbsp; Success
+        </Typography>
+        <Typography sx={text_style}>, Higher </Typography>
+        <Typography color="red" sx={text_style}>
+          &nbsp; Data Quality
+        </Typography>
+      </Grid>
       <Grid
         container
-        className="no-text-select"
+        className="bg-home"
         direction="row"
         columnGap={18}
         sx={{
@@ -201,11 +368,11 @@ export default function Home() {
             paddingLeft: { xs: "50px" },
             display: "flex",
             justifyContent: { xs: "center", lg: "center" },
-            height: { lg: "230px", xs: "130px" },
+            height: { lg: "180px", xs: "130px" },
           }}
         >
           <Typography
-            paddingTop="70px"
+            paddingTop="40px"
             fontSize="36px"
             fontFamily="roboto"
             fontWeight="bold"
@@ -213,6 +380,7 @@ export default function Home() {
             Why Choose Us?
           </Typography>
         </Grid>
+
         <Grid item lg={3} height="150px" marginLeft="20px" sx={block_style}>
           <CurrencyRupeeIcon sx={{ fontSize: "50px" }} /> <br />
           <br />
@@ -231,6 +399,7 @@ export default function Home() {
           trust
         </Grid>
       </Grid>
+
       <Grid container>
         <TypesTable />
       </Grid>
--- src/components/table.js
+++ src/components/table.js
@@ -1,37 +1,10 @@
 import {
-  Table,
-  TableRow,
-  TableHead,
-  TableCell,
-  TableContainer,
   Grid,
-  Typography,
-  TableBody,
 } from "@mui/material";
 import "../App.css";
-import PhotoSizeSelectActualIcon from "@mui/icons-material/PhotoSizeSelectActual";
-import VideoCameraBackIcon from "@mui/icons-material/VideoCameraBack";
-import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
-import TopicIcon from "@mui/icons-material/Topic";
-import DescriptionIcon from "@mui/icons-material/Description";
+const brand = process.env.PUBLIC_URL + "/support.png";
 
 export default function TypesTable() {
-  const type_head = {
-    textAlign: "center",
-    fontWeight: "bold",
-    color: "black",
-    fontSize: "17px",
-    fontFamily: "inherit",
-    letterSpacing: "1.5px",
-    width: "240px",
-  };
-
-  const type_body = {
-    color: "grey",
-    justifyContent: "left",
-    textAlign: "left",
-    display: "flex",
-  };
 
   return (
     <>
@@ -39,146 +12,35 @@ export default function TypesTable() {
         container
         className="no-text-select"
         direction="column"
+        alignItems="center"
         sx={{
-          paddingTop: { lg: "90px", xs: "0px" },
-          height: { lg: "540px", xs: "0px" },
+          pl: "30px",
+          margin: "17px",
+          height: { lg: "690px", xs: "0px" },
         }}
       >
         <Grid
           item
-          lg={5}
-          display={{ xs: "none", lg: "contents" }}
-          sx={{ width: "600px", height: { xs: "2px" } }}
-        >
-          <img
-            alt="brandSupport"
-            height="400px"
-            width="570px"
-            src="https://www.brandsupport.ro/Images/logo2.png"
-          />
-        </Grid>
-        <Grid item lg={7} display={{ xs: "none", lg: "contents" }}>
-          <img
-            alt="brandlogo"
-            width="600px"
-            height="400px"
-            src="https://mynewmicrophone.com/wp-content/uploads/2021/12/mnm_Top_11_Best_External_Hard_Drive_Brands_For_MusicAudio_large.jpg"
-          />
-        </Grid>
+          lg={12}
+          justifyContent="center"
+          display="flex"
+          justifyItems="center"
+          // display={{ xs: "none", lg: "contents" }}
+          sx={{
+            pl: "20px",
+            boxShadow: "0px 6px 13px rgba(0, 0, 0, 0.2)",
+            border: "2px solid black",
+            borderRadius: "100px",
+            height: "870px",
+            width: "1070px",
+            backgroundImage: `url(${brand})`,
+          }}
+        ></Grid>
       </Grid>
-      <Grid item height="120px">
+      <Grid item height="40px">
         .
       </Grid>
-      <Grid container justifyContent={"center"}>
-        <Grid
-          item
-          sx={{ border: "1px solid black", borderRadius: "15px" }}
-          lg={7}
-          md={9}
-          xs={12}
-        >
-          <div
-            style={{
-              fontSize: "26px",
-              fontFamily: "roboto",
-              fontWeight: "bold",
-              letterSpacing: "1.5px",
-              textAlign: "center",
-              backgroundColor: "#a1cef8",
-              textAlign: "center",
-              borderRadius: "15px",
-              paddingTop: "30px",
-              paddingBottom: "30px",
-            }}
-          >
-            Data Support Types
-          </div>
-          <TableContainer
-            sx={{
-              justifyContent: "center",
-              direction: "column",
-              display: "flex",
-            }}
-          >
-            <Table>
-              <TableBody>
-                <TableRow>
-                  <TableCell sx={type_body}>
-                    <Typography sx={type_head}>
-                      <PhotoSizeSelectActualIcon
-                        color="primary"
-                        sx={{ fontSize: "30px" }}
-                      />
-                      &nbsp;&nbsp; Photos
-                    </Typography>
-                  </TableCell>
-                  <TableCell>
-                    JPG/JPEG, TIFF/TIF, PNG, BMP, GIF, PSD, CRW, CR2, NEF, ORF,
-                    SR2, MRW,etc.
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell sx={type_body}>
-                    <Typography sx={type_head}>
-                      <VideoCameraBackIcon
-                        color="primary"
-                        sx={{ fontSize: "30px" }}
-                      />
-                      &nbsp;&nbsp; Videos
-                    </Typography>
-                  </TableCell>
-                  <TableCell>
-                    AVI, MOV, MP4, M4V, 3GP, 3G2, WMV, MKV, ASF, FLV, F4V, SWF,
-                    MPG, RM/RMVB, etc.
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell sx={type_body}>
-                    <Typography sx={type_head}>
-                      <LibraryMusicIcon
-                        color="primary"
-                        sx={{ fontSize: "30px" }}
-                      />
-                      &nbsp;&nbsp; Audios
-                    </Typography>
-                  </TableCell>
-                  <TableCell>
-                    MP3, AIF/AIFF, M4A, WMA, APE, AAC, WAV, FLAC, DTS and other
-                    audio formats.
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell sx={type_body}>
-                    <Typography sx={type_head}>
-                      <TopicIcon color="primary" sx={{ fontSize: "30px" }} />
-                      &nbsp;&nbsp; Files
-                    </Typography>
-                  </TableCell>
-                  <TableCell>
-                    DOC/DOCX, XLS/XLSX, PPT/PPTX, CWK, HTML/HTM, INDD, EPS, RTF,
-                    and so on.
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell sx={type_body}>
-                    <Typography sx={type_head}>
-                      <DescriptionIcon
-                        color="primary"
-                        sx={{ fontSize: "30px" }}
-                      />
-                      &nbsp;&nbsp; RAR/ZIP
-                    </Typography>
-                  </TableCell>
-                  <TableCell>
-                    RAR, ZIP, 7Z, BZ2, ISO, IMG, TAR, GZ, TAZ, TGZ, LHA, DMG and
-                    so much more.
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Grid>
-      </Grid>
+     
     </>
   );
 }
